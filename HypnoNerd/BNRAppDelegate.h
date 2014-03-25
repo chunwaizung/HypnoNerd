22b1924ec741c6c2d280f82fd7ed4fac56cca498
Drew Horn <horn.drew@gmail.com>
2014-03-24 22:46:08 -0500

    Initial Commit

---
+++ HypnoNerd/BNRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BNRAppDelegate.h
+//  HypnoNerd
+//
+//  Created by Drew Horn on 3/24/14.
+//  Copyright (c) 2014 Drew Horn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BNRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HypnoNerd/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HypnoNerd
+//
+//  Created by Drew Horn on 3/24/14.
+//  Copyright (c) 2014 Drew Horn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BNRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BNRAppDelegate class]));
+    }
+}
