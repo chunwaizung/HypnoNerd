980df3ea65daa8d6b27beb3c0671bf971ef68668
Drew Horn <horn.drew@gmail.com>
2014-03-26 20:53:43 -0500

    Added text field that adds labels to view controller
    Added a text field that takes in text and draws 20 labels with that text all over the view controller. Also added the parallax effect for the text.

--- BNRHypnosisViewController.m
+++ BNRHypnosisViewController.m
@@ -9,6 +9,9 @@
 #import "BNRHypnosisViewController.h"
 #import "BNRHypnosisView.h"
 
+@interface BNRHypnosisViewController () <UITextFieldDelegate>
+@end
+
 @implementation BNRHypnosisViewController
 
 - (instancetype)initWithNibName:(NSString *)nibNameOrNil
@@ -36,12 +39,16 @@
     CGRect frame = [UIScreen mainScreen].bounds;
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
     
-    // Create a text field with a border style and add it to the background view
+    // Create a text field and add it to the background view
     CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
     UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
     textField.borderStyle = UITextBorderStyleRoundedRect;
+    textField.placeholder = @"Hypnotize me";
+    textField.returnKeyType = UIReturnKeyDone;
     [backgroundView addSubview:textField];
     
+    // Set the delete for textField to this view controller
+    textField.delegate = self;
     
     // Set backgroundView as the view for this view controller
     self.view = backgroundView;
@@ -53,6 +60,63 @@
     [super viewDidLoad];
     
     NSLog(@"BNRHypnosisViewController loaded its view.");
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField;
+{
+    // Draw the current text all over the view controller
+    [self drawHypnoticMessage:textField.text];
+    
+    // Clear the text in the text field
+    textField.text = @"";
+    
+    // Dismiss the keyboard
+    [textField resignFirstResponder];
+    
+    return YES;
+}
+
+- (void)drawHypnoticMessage:(NSString *)message
+{
+    for (int i = 0; i < 20; i++) {
+        
+        // Create a label and configure color, text
+        UILabel *messageLabel = [[UILabel alloc] init];
+        messageLabel.backgroundColor = [UIColor clearColor];
+        messageLabel.textColor = [UIColor whiteColor];
+        messageLabel.text = message;
+        
+        // Resize the label relative to the text that it's displaying
+        [messageLabel sizeToFit];
+        
+        // Get random cooridnates that fit within the bounds of the view
+        int width = self.view.bounds.size.width - messageLabel.bounds.size.width;
+        int x = arc4random() % width;
+        int height = self.view.bounds.size.height - messageLabel.bounds.size.height;
+        int y = arc4random() % height;
+        
+        // Update the label's frame
+        CGRect frame = messageLabel.frame;
+        frame.origin = CGPointMake(x, y);
+        messageLabel.frame = frame;
+        
+        // Add the label to the heirarchy
+        [self.view addSubview:messageLabel];
+        
+        // Add the "parallax" effect for the message label
+        UIInterpolatingMotionEffect *motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.x"
+                                                                                                    type:UIInterpolatingMotionEffectTypeTiltAlongHorizontalAxis];
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+        
+        motionEffect = [[UIInterpolatingMotionEffect alloc] initWithKeyPath:@"center.y"
+                                                                       type:UIInterpolatingMotionEffectTypeTiltAlongVerticalAxis];
+        
+        motionEffect.minimumRelativeValue = @-25;
+        motionEffect.maximumRelativeValue = @25;
+        [messageLabel addMotionEffect:motionEffect];
+    }
 }
 
 @end
