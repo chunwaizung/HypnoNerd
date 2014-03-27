732933e236176369acad145734160de86807fa00
Drew Horn <horn.drew@gmail.com>
2014-03-26 20:12:38 -0500

    Added UITextFieldView

--- BNRHypnosisViewController.m
+++ BNRHypnosisViewController.m
@@ -36,6 +36,13 @@
     CGRect frame = [UIScreen mainScreen].bounds;
     BNRHypnosisView *backgroundView = [[BNRHypnosisView alloc] initWithFrame:frame];
     
+    // Create a text field with a border style and add it to the background view
+    CGRect textFieldRect = CGRectMake(40, 70, 240, 30);
+    UITextField *textField = [[UITextField alloc] initWithFrame:textFieldRect];
+    textField.borderStyle = UITextBorderStyleRoundedRect;
+    [backgroundView addSubview:textField];
+    
+    
     // Set backgroundView as the view for this view controller
     self.view = backgroundView;
 }
